0512cb0674b80ff149b35dbdd94cb3d9c00c0c47
Wesley Becker <wesley.becker@universo.univates.br>
2024-07-20 23:46:15 -0300

    jest test sequence

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/src/tests/**/*.test.ts'],
+  testMatch: ['**/src/tests/**/*.test.ts', '**/src/tests/**/*.test.js'],
+  testSequencer: './testSequencer.js',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  verbose: true
 };
---
+++ testSequencer.js
@@ -0,0 +1,11 @@
+const Sequencer = require('@jest/test-sequencer').default;
+
+class CustomSequencer extends Sequencer {
+  sort(tests) {
+    // Ordem dos testes
+    const order = ['db.test.js', 'eventos.test.ts'];
+    return tests.sort((a, b) => order.indexOf(a.path.split('/').pop()) - order.indexOf(b.path.split('/').pop()));
+  }
+}
+
+module.exports = CustomSequencer;
